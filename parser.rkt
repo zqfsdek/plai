0157004d40c521571c46906dde63f99594db91d5
zqfsdek <zqfsdek@gmail.com>
2014-05-12 11:10:40 +0800

    parser

---
+++ parser.rkt
@@ -0,0 +1,17 @@
+#lang plai
+(define-type AE
+  [num (n number?)]
+  [add (lhs AE?)
+       (rhs AE?)]
+  [sub (lhs AE?)
+       (rhs AE?)])
+
+(define (parse sexp)
+  (cond
+    [(number? sexp) (num sexp)]
+    [(list? sexp)
+     (case (first sexp)
+       [(+) (add (parse (second sexp)) 
+                 (parse (third sexp)))]
+       [(-) (sub (parse (second sexp)) 
+                 (parse (third sexp)))])]))
